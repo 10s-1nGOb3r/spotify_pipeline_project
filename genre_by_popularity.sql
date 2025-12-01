7d148dd3864db416a37d5ca28b95796fcf7299d1
10s-1nGOb3r <rizkan.almaududy@gmail.com>
2025-12-01 20:26:42 +0700

    Create view_genre_pop to display genre popularity

---
+++ genre_by_popularity.sql
@@ -0,0 +1,13 @@
+CREATE 
+    ALGORITHM = UNDEFINED 
+    DEFINER = `root`@`localhost` 
+    SQL SECURITY DEFINER
+VIEW `view_genre_pop` AS
+    SELECT 
+        `genr`.`name` AS `genre`,
+        ROUND(AVG(`art`.`popularity`), 0) AS `popularity`
+    FROM
+        ((`artists` `art`
+        JOIN `artist_genres` `arge` ON ((`arge`.`artist_id` = `art`.`spotify_id`)))
+        JOIN `genres` `genr` ON ((`genr`.`id_genre` = `arge`.`genre_id`)))
+    GROUP BY `genr`.`name`
